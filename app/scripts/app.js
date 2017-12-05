7c90a15c4570cf34dee0e8ecff9e57248e03ace3
artobay <aliyarfatemi@yahoo.com>
2017-12-05 15:56:38 -0800

    checkpoint 5 is completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,6 +12,7 @@
 		 $stateProvider
          .state('landing', {
              url: '/',
+			 controller: 'LandingCtrl as Landing'
              templateUrl: '/templates/landing.html'
          })
 		.state('album', {
@@ -21,6 +22,7 @@
 		 
 		  .state('collection', {
         url: '/collection',
+		controller: 'CollectionCtrl as collection',	 
         templateUrl: '/templates/collection.html'
       });
      }
